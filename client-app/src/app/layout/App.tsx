00ac221ad7164a9a042e03ed37c6cf50da57fb51
Rinae Tshibubudze <vharhee@gmail.com>
2022-09-30 08:44:31 +0200

    Folder structure

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,6 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import axios from 'axios';
 import { Header, List } from 'semantic-ui-react';
 
@@ -10,10 +8,9 @@ const [activities, setActivities] = useState([]);
 
 useEffect(() => {
   axios.get("http://localhost:5000/api/activities").then(response => {
-    console.log(response);
     setActivities(response.data);
   });
-}, []);
+}, [])
 
   return (
     <div>
--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './app/layout/styles.css';
+import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+      <App />
+ );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
