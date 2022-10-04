eaaec222b91e726bf11284b85fa9781463414054
Rinae Tshibubudze <vharhee@gmail.com>
2022-10-04 08:52:36 +0200

    Setting Axios to talk to the API

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,9 +1,9 @@
 import React, { Fragment, useEffect, useState } from 'react';
-import axios from 'axios';
 import { Container} from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import Navbar from './Navbar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+import agent from '../api/agent';
 
 
 function App() {
@@ -12,8 +12,8 @@ const [selectedActivity, setSelectedActivity] = useState<Activity | undefined >(
 const [editMode, setEditMode] = useState(false);
 
 useEffect(() => {
-  axios.get<Activity[]>("http://localhost:5000/api/activities").then(response => {
-    setActivities(response.data);
+  agent.Activities.list().then(response => {
+    setActivities(response);
   });
 }, [])
 
