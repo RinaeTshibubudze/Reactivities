e7b53d06fedaa765a451c4de4836a60f2c69479b
Rinae Tshibubudze <vharhee@gmail.com>
2022-10-10 12:16:20 +0200

    Remove runInAction from the store

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,4 +1,4 @@
-import { makeAutoObservable, runInAction } from "mobx";
+import { makeAutoObservable } from "mobx";
 import { Activity } from '../models/activity';
 import agent from '../api/agent';
 
@@ -15,23 +15,22 @@ export default class ActivityStore {
     }
 
     loadActivities = async () => {
-        this.loadingInitial = true;
+        this.setLoadingInitial(true);
         try {
             const activities = await agent.Activities.list();
-            runInAction(() => {
-                activities.forEach(activity => {
-                    activity.date = activity.date.split('T')[0];
-                    this.activities.push(activity);
-                })
-                this.loadingInitial = false;
+
+            activities.forEach(activity => {
+                activity.date = activity.date.split('T')[0];
+                this.activities.push(activity);
             })
+            this.setLoadingInitial(false);
 
         } catch (error) {
             console.log(error);
-            runInAction(() => {
-                this.loadingInitial = false;
-            })
+            this.setLoadingInitial(false);
         }
     }
-
+    setLoadingInitial = (state: boolean) => {
+        this.loadingInitial = state;
+    }
 }
