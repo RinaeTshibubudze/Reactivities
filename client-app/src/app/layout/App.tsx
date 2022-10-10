fd01974af62d0528a6df88bf88f92946f1575d95
Rinae Tshibubudze <vharhee@gmail.com>
2022-10-10 14:58:27 +0200

    Select an activity using Mobx

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -22,22 +22,6 @@ useEffect(() => {
   activityStore.loadActivities();
 }, [activityStore])
 
-function handleSelectActivity(id: string){
-  setSelectedActivity(activities.find(x => x.id === id))
-}
-function handleCancelSelectActivity(){
-  setSelectedActivity(undefined);
-}
-
-function handleFormOpen(id?: string) {
-  id ? handleSelectActivity(id) : handleCancelSelectActivity();
-  setEditMode(true);
-}
-
-function handleFormClose(){
-  setEditMode(false)
-}
-
 function handleCreateOrEditActivitity(activity: Activity){
   setSubmitting(true);
   if (activity.id) {
@@ -70,16 +54,10 @@ if (activityStore.loadingInitial) return <LoadingComponent content='Loading app'
 
   return (
     <>
-      <Navbar openForm={handleFormOpen} />
+      <Navbar  />
       <Container style={{marginTop: '7em'}}>
           <ActivityDashboard 
           activities={activityStore.activities} 
-          selectedActivity={selectedActivity}
-          selectActivity={handleSelectActivity}
-          cancelSelectActivity={handleCancelSelectActivity}
-          editMode={editMode}
-          openForm={handleFormOpen}
-          closeForm={handleFormClose}
           createOrEdit={handleCreateOrEditActivitity}
           deleteActivity={handleDeleteActivity}
           submitting={submitting}
