585f0e6c67778bac5fc49cf39d51b66834ed9eae
Rinae Tshibubudze <vharhee@gmail.com>
2022-10-10 12:08:51 +0200

    Refactoring Using Mobx and deal with Strict mode

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,14 +1,37 @@
-import { makeAutoObservable } from "mobx";
+import { makeAutoObservable, runInAction } from "mobx";
+import { Activity } from '../models/activity';
+import agent from '../api/agent';
 
 export default class ActivityStore {
 
-    title = 'Hello from MobX!';
+    activities: Activity[] = [];
+    selectedActivity: Activity | null = null;
+    editMode = false;
+    loading = false;
+    loadingInitial = false;
 
     constructor() {
         makeAutoObservable(this)
     }
 
-    setTitle = () => {
-        this.title = this.title + '!'
+    loadActivities = async () => {
+        this.loadingInitial = true;
+        try {
+            const activities = await agent.Activities.list();
+            runInAction(() => {
+                activities.forEach(activity => {
+                    activity.date = activity.date.split('T')[0];
+                    this.activities.push(activity);
+                })
+                this.loadingInitial = false;
+            })
+
+        } catch (error) {
+            console.log(error);
+            runInAction(() => {
+                this.loadingInitial = false;
+            })
+        }
     }
+
 }
