9cf27111ede043af105c833841f1f0b1e8077641
Rinae Tshibubudze <vharhee@gmail.com>
2022-10-12 14:21:42 +0200

    Adding routes

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,26 +1,20 @@
-import React, { Fragment, useEffect} from 'react';
+import React from 'react';
 import { Container} from 'semantic-ui-react';
 import Navbar from './Navbar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-import LoadingComponent from './LoadingComponets';
-import { useStore } from '../stores/store';
 import { observer } from 'mobx-react-lite';
+import { Route } from 'react-router-dom';
+import HomePage from '../../features/home/Homepage';
+import ActivityForm from '../../features/activities/form/ActivityForm';
 
 function App() {
-  const {activityStore} = useStore();
-
-useEffect(() => {
-  activityStore.loadActivities();
-}, [activityStore])
-
-
-if (activityStore.loadingInitial) return <LoadingComponent content='Loading app' />
-
   return (
     <>
       <Navbar  />
       <Container style={{marginTop: '7em'}}>
-          <ActivityDashboard />
+          <Route exact path='/' component={HomePage} />
+          <Route path='/activities' component={ActivityDashboard} />
+          <Route path='/createActivity' component={ActivityForm} />
       </Container>
     </>
   );
--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Grid} from "semantic-ui-react";
 import ActivityList from "./ActivityList";
 import ActivityDetails from '../details/ActivityDetails';
 import ActivityForm from "../form/ActivityForm";
 import { useStore } from '../../../app/stores/store';
 import { observer } from 'mobx-react-lite';
+import LoadingComponent from "../../../app/layout/LoadingComponets";
 
 
 
@@ -13,6 +14,13 @@ export default observer(function ActivityDashboard() {
     
     const {activityStore}  = useStore();
     const {selectedActivity, editMode} = activityStore; 
+
+        useEffect(() => {
+        activityStore.loadActivities();
+        }, [activityStore])
+
+
+    if (activityStore.loadingInitial) return <LoadingComponent content='Loading app' />
     return (
         <Grid>
             <Grid.Column width='10'>
