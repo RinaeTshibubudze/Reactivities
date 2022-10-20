fe7780aacc4750924e7a5e47439214d161ed03f8
Rinae Tshibubudze <vharhee@gmail.com>
2022-10-20 11:08:59 +0200

    Create Details components

--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -1,6 +1,6 @@
-import React, { SyntheticEvent, useState } from "react";
+import { SyntheticEvent, useState } from "react";
 import { Link } from "react-router-dom";
-import { Button, Icon, Item, Label, Segment } from "semantic-ui-react";
+import { Button, Icon, Item, Segment } from "semantic-ui-react";
 import { Activity } from '../../../app/models/activity';
 import { useStore } from "../../../app/stores/store";
 
@@ -11,7 +11,7 @@ interface Props {
 export default function ActivityListItem({activity}: Props) {
 
     const {activityStore} = useStore();
-    const {deleteActivity, loading} = activityStore;
+    const {deleteActivity} = activityStore;
     const [target, setTarget] = useState('')
 
     function handleActivityDelete(e: SyntheticEvent<HTMLButtonElement>, id: string) {
--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,9 +1,12 @@
 import React, { useEffect } from "react";
-import { Button, Card, Image } from "semantic-ui-react";
+import { Button, Card, Grid, Image } from "semantic-ui-react";
 import { useStore } from '../../../app/stores/store';
 import LoadingComponent from '../../../app/layout/LoadingComponets';
 import { Link, useParams } from "react-router-dom";
 import { observer } from 'mobx-react-lite';
+import ActivityDetailedHeader from "./ActivityDetailedHeader";
+import ActivityDetailedInfo from './ActivityDetailedInfo';
+import ActivityDetailedChat from "./ActivityDetailedChat";
 
 
 export default observer(function ActivityDetails() {
@@ -18,23 +21,15 @@ export default observer(function ActivityDetails() {
 
     if (loadingInitial || !activity) return < LoadingComponent/>;
     return (
-        <Card fluid>
-            <Image src={`/assets/categoryImages/${activity.category}.jpg`} />
-            <Card.Content>
-            <Card.Header>{activity.title}</Card.Header>
-            <Card.Meta>
-                <span>{activity.date} </span>
-            </Card.Meta>
-            <Card.Description>
-                {activity.description}
-            </Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-            <Button.Group widths='2'>
-                <Button as={Link} to={`/manage/${activity.id}`} basic color="blue" content='Edit'/>
-                <Button as={Link} to='/activities' basic color="grey" content='Cancel'/>
-            </Button.Group>
-    </Card.Content>
-  </Card>
+        <Grid>
+            <Grid.Column width={10}>
+                <ActivityDetailedHeader />
+                <ActivityDetailedInfo />
+                <ActivityDetailedChat />
+            </Grid.Column>
+            <Grid.Column width={6}>
+                <ActivityDetailedChat />
+            </Grid.Column>
+        </Grid>
     )
 })
