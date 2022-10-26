2a94c00be3715795fb0b6836b96f41e0f6f630e6
Rinae Tshibubudze <vharhee@gmail.com>
2022-10-26 10:28:39 +0200

    Handling Api error responses on Create and list

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -14,9 +14,9 @@ namespace API.Controllers
     public class ActivitiesController : BaseApiController
     {
         [HttpGet]
-        public async Task<ActionResult<List<Activity>>> GetActivities()
+        public async Task<IActionResult> GetActivities()
         {
-            return await Mediator.Send(new List.Query());
+            return HandleResult(await Mediator.Send(new List.Query()));
         }
 
         [HttpGet("{id}")]
@@ -28,7 +28,7 @@ namespace API.Controllers
         [HttpPost]
         public async Task<IActionResult> CreateActivity(Activity activity)
         {
-            return Ok(await Mediator.Send(new Create.Command { Activity = activity }));
+            return HandleResult(await Mediator.Send(new Create.Command { Activity = activity }));
         }
 
         [HttpPut("{id}")]
